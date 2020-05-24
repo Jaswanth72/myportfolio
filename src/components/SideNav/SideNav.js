c7071807c8c55895d1b60a3728391d8bfa024ff1
Jaswanth sai <n.jaswanthsai@gmail.com>
2020-05-24 15:30:27 +0530

    my portfolio

---
+++ src/components/SideNav/SideNav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './sidenav.css';
+
+export const SideNav = ({ handleSectionClick }) => {
+    return (
+        <div className="sidenav">
+            <img
+                src="https://avatars2.githubusercontent.com/u/55351107?s=400&u=cbd45f43066025f73a0f9c8d9889c3b4e6637ca9&v=4"
+                className="avatar"
+                alt="avatar"
+            />
+            <h2 className="main-name">Jaswanth Sai</h2>
+            <p className="subtitle">"A Cadet Who Codes"</p>
+            <div className="sections-list">
+                <p className="section-list-element" onClick={() => handleSectionClick("about")}>About Me</p>
+                <p className="section-list-element" onClick={() => handleSectionClick("skills")}>Skills</p>
+                <p className="section-list-element" onClick={() => handleSectionClick("projects")}>Projects</p>
+            </div>
+        </div>
+    );
+};
