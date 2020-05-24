f791f154eaa40d1335b931d9368ac918663bbc4a
Jaswanth sai <n.jaswanthsai@gmail.com>
2020-05-24 15:32:06 +0530

    Updates

---
+++ precache-manifest.d9e674d1dbd8d9304da3403e854dde7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47643c6a2af245b03f5f301c99b8db1b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4a137fadab1d5615f752",
+    "url": "/portfolio/static/css/main.d42ce043.chunk.css"
+  },
+  {
+    "revision": "da565a7dba9992f5d07e",
+    "url": "/portfolio/static/js/2.9cb5cabf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.9cb5cabf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a137fadab1d5615f752",
+    "url": "/portfolio/static/js/main.aa9a2c11.chunk.js"
+  },
+  {
+    "revision": "ce977d6080640af7189b",
+    "url": "/portfolio/static/js/runtime-main.22e213d8.js"
+  }
+]);
